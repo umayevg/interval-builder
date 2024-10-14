30fa9f99c8943f9fa5e5598768b8765003b9a15f
umayevg <beamyg@gmail.com>
2024-10-14 19:31:56 +0200

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { I18nextProvider } from 'react-i18next'
 import TimerConstructor from './components/TimerConstructor'
 import i18n from './translations/i18n'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
 	return (
 		<>
 			<I18nextProvider i18n={i18n}>
 				<TimerConstructor />
+
+				<Analytics />
 			</I18nextProvider>
 		</>
 	)
